558546161e49ae601d45b9c41b794ce15b60a207
Nasiim X <nasiim.nsmlp@gmail.com>
2023-01-14 13:33:16 +0600

    added blog categories

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -1,15 +1,24 @@
 from django import forms 
-from .models import Post 
+from .models import Post, Category
+
+#catgry = [('Coding','Coding'),('Sports','Sports'),('Entertainment','Entertainment'),('Travelling & Guides','Travelling & Guides')]
+choices = Category.objects.all().values_list('name','name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post 
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'This is Title placeholder'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add a Title tag'}),
             'author': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Select Author'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'placeholder': 'Select Category'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your post here...'}),
         }
 
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post 
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -18,12 +18,18 @@ class ArticleDetailView(DetailView):
     model = Post 
     template_name = 'article_details.html'
 
-
 class AddPostView(CreateView):
     model = Post 
     form_class = PostForm 
     template_name = 'add_post.html'
     #fields = '__all__'
+    #fields = ('title', 'body')
+
+class AddCategoryView(CreateView):
+    model = Category  
+    #form_class = PostForm 
+    template_name = 'add_category.html'
+    fields = '__all__'
     #fields = ('title', 'body')
 
 class UpdatePostView(UpdateView):
