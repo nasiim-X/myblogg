c0041b7e00829366081c02a081f5197b2182c09a
Nasiim X <nasiim.nsmlp@gmail.com>
2023-01-16 17:11:15 +0600

    likes count

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Category
 from .forms import PostForm, EditForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 
 #def home(request):
     #return render(request, 'home.html', {})
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id = request.POST.get('post_id'))
-
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 class HomeView(ListView):
     model = Post 
@@ -40,7 +42,11 @@ class ArticleDetailView(DetailView):
     def get_context_data(self, *args, **kwarks):
         cat_menu = Category.objects.all()
         context = super(ArticleDetailView, self).get_context_data(*args, **kwarks)
+        
+        stuff = get_object_or_404(Post, id = self.kwargs['pk'])
+        total_likes = 
         context["cat_menu"] = cat_menu
+        context["total_likes"] = total_likes 
         return context 
 
 
