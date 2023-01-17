60c9719777c33c100dbc9b91ac70f095f078da97
Nasiim X <nasiim.nsmlp@gmail.com>
2023-01-17 22:10:30 +0600

    added fields to registration forms

--- ablog/members/forms.py
+++ ablog/members/forms.py
@@ -12,4 +12,9 @@ class SingUpForm(UserCreationForm):
         fields = ('username','first_name', 'last_name', 'email', 'password1', 'password2')
 
 
-   
+    def __init__(self, *args, **kwargs):
+        super(SingUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
