617c78a1698add91bdb43ee05eafadfc3d2642f4
Nasiim X <nasiim.nsmlp@gmail.com>
2023-01-21 19:37:25 +0600

    pass success URL

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views import generic  
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm,PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SingUpForm, EditProfileForm
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm 
+    success_url = reverse_lazy('password_success')
+    #success_url = reverse_lazy('home')
+
+def password_success(request):
+    return render(request,'registration/password_success.html',{})
 
 class UserRegisterView(generic.CreateView):
     form_class = SingUpForm
