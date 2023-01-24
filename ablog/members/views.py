6e2f4452d00cc2c0a759fae30682b476300a1df1
Nasiim X <nasiim.nsmlp@gmail.com>
2023-01-25 02:38:11 +0600

    added fields

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -4,13 +4,15 @@ from django.views.generic import DetailView,CreateView
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm,PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SingUpForm, EditProfileForm, PasswordChangingForm
+from .forms import SingUpForm,EditProfileForm,PasswordChangingForm,ProfilePageForm
 from theblog.models import  Profile
 
 
 class CreateProfilePageView(CreateView):
     model = Profile
+    form_class = ProfilePageForm
     template_name = "registration/create_user_profile_page.html"
+    #fields = '__all__'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
