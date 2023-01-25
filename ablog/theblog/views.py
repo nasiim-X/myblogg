a8df530174b58797f77be1cf780fb9e0195272a4
Nasiim X <nasiim.nsmlp@gmail.com>
2023-01-26 02:55:42 +0600

    working on comments

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post, Category
-from .forms import PostForm, EditForm
+from .models import Post, Category,Comment
+from .forms import PostForm, EditForm,CommentForm
 from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
 
@@ -71,6 +71,15 @@ class AddPostView(CreateView):
     #fields = ('title', 'body')
 
 
+class AddCommentView(CreateView):
+    model = Comment 
+    form_class = CommentForm
+    template_name = 'add_comment.html'
+    #fields = '__all__'
+
+    success_url = reverse_lazy('home')
+
+
 class AddCategoryView(CreateView):
     model = Category  
     #form_class = PostForm 
@@ -78,11 +87,13 @@ class AddCategoryView(CreateView):
     fields = '__all__'
     #fields = ('title', 'body')
 
+
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
     #fields = ['title', 'title_tag',  'body']
+
 
 class DeletePostView(DeleteView):
     model = Post 
